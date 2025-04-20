f31b8b53021295d2994444c933ff69a093ff1158
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-04-20 09:44:51 +0300

    🔥 Updated: Plugin meta info.

--- includes/Controllers/PluginMeta/MetaInfo.php
+++ includes/Controllers/PluginMeta/MetaInfo.php
@@ -1,11 +1,11 @@
 <?php
-namespace RECAPADDON\Controllers\PluginMeta;
+namespace UMVADDON\Controllers\PluginMeta;
 
 /**
  * Class displays options panel, addons, documentation links below the plugin information.
  *
  * @since: 1.1.0
- * @package RECAPADDON
+ * @package UMVADDON
  */
 class MetaInfo {
 
@@ -26,17 +26,22 @@ class MetaInfo {
      */
 	public function get_meta_links( $links, $file ) {
 
-		if ( strpos( $file, RECAPADDON_PLUGIN_ROOT_FILE ) !== false && is_plugin_active( $file ) ) {
+		if ( strpos( $file, UMVADDON_PLUGIN_ROOT_FILE ) !== false && is_plugin_active( $file ) ) {
 
 			// nt = 1 // new tab.
 			$additional_links = [
 				[
-					'title' => esc_html__( 'Options Panel', 'bpvm-recap' ),
-					'url'   => admin_url( 'admin.php?page=bwl-pvm_option_panel#bpvm_recaptcha_options' ), //phpcs:ignore
+					'title' => esc_html__( 'Options Panel', 'bpvm-umv' ),
+					'url'   => admin_url( 'admin.php?page=bwl-pvm_option_panel#bpvm_umv_options' ), //phpcs:ignore
 				],
 				[
-					'title' => esc_html__( 'Docs', 'bpvm-recap' ),
-					'url'   => 'https://xenioushk.github.io/docs-plugins-addon/bpvm-addon/recap/index.html',
+					'title' => esc_html__( 'Docs', 'bpvm-umv' ),
+					'url'   => 'https://xenioushk.github.io/docs-plugins-addon/bpvm-addon/umva/index.html',
+					'nt'    => 1,
+				],
+				[
+					'title' => esc_html__( 'Support', 'bpvm-umv' ),
+					'url'   => 'https://codecanyon.net/item/undo-my-vote-addon-for-bwl-pro-voting-manager/32986128/support',
 					'nt'    => 1,
 				],
 
